0b3e48af036ad0c36f78df5c047889eb061e4300
Mariah Mirzayan <mmirzayan06@gmail.com>
2022-02-26 12:04:56 -0800

    initial commit

---
+++ analytics.py
@@ -0,0 +1,29 @@
+from time import sleep
+import flask
+import pymongo
+import mysql.connector
+
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["analytics"]
+mycol = mydb["analytics"]
+
+
+while True:
+    mysqldb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="shayanA-123"
+    )
+    mycursor = mysqldb.cursor()
+    mycursor.execute("SELECT * FROM numbers.nums")
+    myresult = mycursor.fetchall()
+    numList = []
+    for x in myresult:
+        numList.append(x[0])
+    mycol.delete_many({})
+    if(len(numList)>0):
+        mycol.insert_one({'avg': sum(numList)/len(numList)})
+    else:
+        mycol.insert_one({'avg': 0})
+    sleep(2.0)
---
+++ controller/numbers_controller.js
@@ -0,0 +1,96 @@
+let database = require("../database");
+const mysql = require("mysql");
+const { MongoClient } = require("mongodb");
+
+let numbersController = {
+  list: async (req, res) => {
+    const con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "shayanA-123",
+      database: "numbers",
+    });
+    con.connect(function (err) {
+      if (err) throw err;
+      con.query(`select * from nums;`, function (err, result, fields) {
+        if (err) throw err;
+        res.render("number/index", { numbers: JSON.parse(JSON.stringify(result)) });
+      });
+    });
+  },
+
+  new: (req, res) => {
+    res.locals.page = "create";
+    res.render("number/create");
+  },
+
+  // Create a reminder
+  create: (req, res) => {
+    let description = req?.body?.description?.split(" ");
+    let desc = [];
+    description.forEach((num) => {
+      desc.push(`(${num})`);
+    });
+    const con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "shayanA-123",
+      database: "numbers",
+    });
+
+    con.connect(function (err) {
+      if (err) throw err;
+      con.query(`insert into nums (numbers) values ${desc.join(",")}`, function (err, result) {
+        if (err) throw err;
+        console.log("Done: " + result);
+      });
+    });
+
+    res.redirect("/numbers");
+  },
+
+  delete: (req, res) => {
+    console.log("called");
+    const con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "shayanA-123",
+      database: "numbers",
+    });
+
+    con.connect(function (err) {
+      if (err) throw err;
+      con.query(`delete from nums`, function (err, result) {
+        if (err) throw err;
+        console.log("Done: " + result);
+      });
+    });
+
+    res.redirect("/numbers");
+  },
+  listAnalytics: async (req, res) => {
+    const url = "mongodb://localhost:27017";
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+      if (err) throw err;
+
+      const db = client.db("analytics");
+
+      db.collection("analytics")
+        .find({})
+        .toArray()
+        .then((docs) => {
+          console.log(docs[0].avg);
+          res.locals.page = "analytics";
+          res.render("number/analytics", { avg: docs[0].avg });
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          client.close();
+        });
+    });
+  },
+};
+
+module.exports = numbersController;
